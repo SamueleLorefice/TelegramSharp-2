d6bc5af181a235e4283fddc21555f276e876852b
Samuele Lorefice <aironenerowork@gmail.com>
2016-12-11 20:35:02 +0100

    Fixed some errors, moved all references to mono runtimes

--- TelegramSharp.Core/Bot.cs
+++ TelegramSharp.Core/Bot.cs
@@ -1,7 +1,10 @@
-﻿namespace TelegramSharp.Core
+﻿using TelegramSharp.Objects;
+namespace TelegramSharp.Core
 {
     public class Bot
     {
-
+		public string BotName;
+		public string token;
+		public User BotData;
     }
 }
